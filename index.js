6593e509dcf639dc19e0ec2ee67c5a8eb769396f
ufoo68 <mitarou.yuutarou@gmail.com>
2020-03-14 16:59:48 +0900

    Implemnt line push message

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
+const core = require('@actions/core')
+const github = require('@actions/github')
+const line = require('@line/bot-sdk')
+const { buildReplyText } = require('line-message-builder')
+
+const client = new line.Client()
 
 try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
+  const userId = core.getInput('user-id')
+  const message = core.getInput('message')
+  client.pushMessage(userId, buildReplyText(message))
 } catch (error) {
-  core.setFailed(error.message);
+  core.setFailed(error.message)
 }
